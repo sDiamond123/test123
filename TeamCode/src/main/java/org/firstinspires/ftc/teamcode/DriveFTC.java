c15d52025eb3f6f27b0b0d24421e01b5680b8870
sDiamond123 <sdiamond21@sidwell.edu>
2019-11-17 13:26:29 -0500

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveFTC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveFTC.java
@@ -3,30 +3,27 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class DriveFTC extends OpMode {
-    //boolean reverse=false;
     double speed=1;
-    double w1=0;
-    double w2=0;
-    DcMotor leftFront, leftRear, rightFront, rightRear, linSlide1, linSlide2;
-    Servo wrist1, wrist2;
-    DcMotor leftFront, leftRear, rightFront, rightRear, forkOneRight, forkOneLeft, forkTwoLeft, forkTwoRight;
-    double speedLeft=0;
-    double speedRight=0;
+    double wPos=0;
+    double [] wPositions={};
+    int wIndex;
+    double la=1;
+    double lspeed=0;
+    Servo wrist;
+    DcMotor leftFront, leftRear, rightFront, rightRear, linearSlide;
     @Override
     public void init() {
-        forkOneLeft=hardwareMap.dcMotor.get("fOL");
-        forkOneRight=hardwareMap.dcMotor.get("fOR");
-        forkTwoLeft=hardwareMap.dcMotor.get("fTL");
-        forkTwoRight=hardwareMap.dcMotor.get("fTR");
+        linearSlide=hardwareMap.dcMotor.get("linSlide");
         leftFront = hardwareMap.dcMotor.get("leftFront");
         leftRear = hardwareMap.dcMotor.get("leftRear");
         rightFront = hardwareMap.dcMotor.get("rightFront");
         rightRear = hardwareMap.dcMotor.get("rightRear");
-
+        wrist=hardwareMap.servo.get("wrist");
         //leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         //rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
         //rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -45,22 +42,71 @@ public class DriveFTC extends OpMode {
         if (gamepad1.dpad_down) {
             speed = .2;
         }
-            double x = gamepad1.left_stick_x * speed; // * (reverse ? -1 : 1);
-            double y = -gamepad1.left_stick_y * speed; // * (reverse ? -1 : 1);
-            double turn =-1* gamepad1.right_stick_y * speed;
-            leftFront.setPower(x + y + turn);
-            rightFront.setPower(-x + y - turn);
-            leftRear.setPower(-x + y + turn);
-            rightRear.setPower(x + y - turn);
+        double x = gamepad1.left_stick_x * speed; // * (reverse ? -1 : 1);
+        double y = -gamepad1.left_stick_y * speed; // * (reverse ? -1 : 1);
+        double turn =-1* gamepad1.right_stick_y * speed;
+        leftFront.setPower(x + y + turn);
+        rightFront.setPower(-x + y - turn);
+        leftRear.setPower(-x + y + turn);
+        rightRear.setPower(x + y - turn);
         //driving code end
 
         //arm code start
-        double armSpeed1=gamepad2.left_stick_y;
-        double armSpeed2=gamepad2.right_stick_y;
-        forkOneLeft.setPower(armSpeed1);
-        forkOneRight.setPower(armSpeed1);
-        forkTwoLeft.setPower(armSpeed2);
-        forkTwoRight.setPower(armSpeed2);
+        if (gamepad2.dpad_up) {
+            la = 1;
+        }
+        if (gamepad2.dpad_left || gamepad2.dpad_right) {
+            la = .5;
+        }
+        if (gamepad2.dpad_down) {
+            la = .2;
+        }
+        if (gamepad2.a)
+        {
+            wIndex++;
+            wIndex%=wPositions.length;
+            wPos=(wPositions[wIndex]);
+        }
+        if (gamepad2.b)
+        {
+            wIndex--;
+            if (wIndex<0)
+            {
+                wIndex=-wIndex;
+            }
+            wIndex%=wPositions.length;
+            wPos=(wPositions[wIndex]);
+        }
+        if (gamepad2.x)
+        {
+            wPos+=.01;
+            wPos%=1;
+        }
+        if (gamepad2.y)
+        {
+            wPos-=.01;
+            if (wPos<0)
+            {
+                wPos=-wPos;
+            }
+            wPos%=1;
+        }
+        wrist.setPosition(wPos);
+
+        if (gamepad2.left_stick_y>0)
+        {
+            lspeed+=.01*la;
+            linearSlide.setPower(lspeed);
+        }
+        if (gamepad2.left_stick_y<0)
+        {
+            lspeed-=.01*la;
+            if (lspeed<0)
+            {
+                lspeed=-lspeed;
+            }
+            linearSlide.setPower(lspeed);
+        }
         //arm code end
     }
 }
